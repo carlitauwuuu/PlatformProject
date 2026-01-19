74ca199f0f178a9f5ad79631fe044201c8cbdd14
carlitauwuuu <mivicarlaa@gmail.com>
2026-01-19 17:01:11 +0100

    cambios niv

--- Assets/Scripts/buttoncontroller.cs
+++ Assets/Scripts/buttoncontroller.cs
@@ -8,6 +8,7 @@ public class buttoncontroller : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         buttondoor.GetComponent<Collider2D>().enabled = true;
+        //pasar puerta a color blanco checkear script de gancho
         GetComponent<SpriteRenderer>().sprite = DoorPushed;
     }
 }
